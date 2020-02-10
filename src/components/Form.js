9b67108ffd63c2a3a7d4cda6c01305e42594c751
Bojana Corbic <bojana.corbic@quantox.com>
2020-02-10 16:08:28 +0100

    formik

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,61 +1,11 @@
 import React, { useState } from "react";
+import { Formik } from "formik";
 
 function Form() {
-  const [formInput, setFormInput] = useState({
-    email: "",
-    password: "",
-    id: ""
-  });
   const [isLogin, setIsLogin] = useState(false);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    //localStorage.setItem("user", JSON.stringify(formInput));
-
-    if (formInput.email === "" && formInput.password === "") {
-      return setIsLogin(false);
-    }
-
-    var oldInput = JSON.parse(localStorage.getItem("input")) || [];
-
-    var newInput = {
-      ...formInput
-    };
-
-    oldInput.push(newInput);
-
-    localStorage.setItem("input", JSON.stringify(oldInput));
-    setIsLogin(true);
-
-    function validateEmail(mail) {
-      if (
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formInput.email)
-      ) {
-        return true;
-      }
-      alert("You have entered an invalid email address!");
-      setIsLogin(false);
-      return false;
-    }
-    validateEmail(formInput.email);
-  };
-
-  const handleChange = e => {
-    const value = e.target.value;
-    setFormInput({
-      ...formInput,
-      [e.target.name]: value,
-      id: Math.random()
-    });
-  };
 
   const handleClick = () => {
     setIsLogin(false);
-    setFormInput({
-      email: "",
-      password: "",
-      id: ""
-    });
   };
 
   return (
@@ -70,40 +20,79 @@ function Form() {
           </div>
         </div>
       ) : (
-        <form className="form" onSubmit={handleSubmit}>
-          <span className="login">Login</span>
-          <div className="login-text">
-            <input
-              type="text"
-              className="login-input"
-              name="email"
-              onChange={handleChange}
-            />
-            <span>Email</span>
-          </div>
-          <div className="login-password">
-            <input
-              type="password"
-              className="login-input"
-              name="password"
-              onChange={handleChange}
-            />
-            <span>Password</span>
-          </div>
-          {localStorage.length < 1 ? (
-            <div className="login-button">
-              <button type="submit" className="button">
-                Register
-              </button>
-            </div>
-          ) : (
-            <div className="login-button">
-              <button type="submit" className="button">
-                Login
-              </button>
-            </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={values => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = <h4 className="error">Required email</h4>;
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = <h4 className="error">Invalid email address</h4>;
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            var oldInput = JSON.parse(localStorage.getItem("input")) || [];
+
+            var newInput = {
+              ...values
+            };
+
+            oldInput.push(newInput);
+
+            localStorage.setItem("input", JSON.stringify(oldInput));
+            setIsLogin(true);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+          }) => (
+            <form onSubmit={handleSubmit} className="form">
+              <span className="login">Login</span>
+              <div className="login-text">
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  className="login-input"
+                />
+                <span>Email</span>
+              </div>
+              {errors.email && touched.email && errors.email}
+              <div className="login-password">
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  className="login-input"
+                />
+                <span>Password</span>
+              </div>
+              {errors.password && touched.password && errors.password}
+              <div className="login-button">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="button"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           )}
-        </form>
+        </Formik>
       )}
     </>
   );
